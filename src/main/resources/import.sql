e4a6bbf4a374a15452b63971c443117e2f2a56cf
Omar-Codess <omartaki8@gmail.com>
2023-03-29 18:54:15 +0200

    push generico

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `pizzas` (`id`, `description`, `name`, `price`) VALUES (NULL, `Pomodoro, mozzarella`, `Pizza Margherita`, `5.00`);
-INSERT INTO `pizzas` (`id`, `description`, `name`, `price`) VALUES (NULL, `Pomodoro, mozzarella, capperi, acciughe`, `Pizza Romana`, `6.00`);
-INSERT INTO `pizzas` (`id`, `description`, `name`, `price`) VALUES (NULL, `Pomodoro, frutti di mare`, `Pizza Frutti di mare`, `7.50`);
-INSERT INTO `pizzas` (`id`, `description`, `name`, `price`) VALUES (NULL, `Pomodoro, mozzarella, patatine fritte`, `Pizza Patatine`, `5.50`);
+INSERT INTO `pizzas` (`description`, `name`, `price`) VALUES ( 'Pomodoro, mozzarella', 'Pizza Margherita', 5.00);
+INSERT INTO `pizzas` (`description`, `name`, `price`) VALUES ( 'Pomodoro, mozzarella, capperi, acciughe', 'Pizza Romana', 6.00);
+INSERT INTO `pizzas` (`description`, `name`, `price`) VALUES ( 'Pomodoro, frutti di mare', 'Pizza Frutti di mare', 7.50);
+INSERT INTO `pizzas` (`description`, `name`, `price`) VALUES ( 'Pomodoro, mozzarella, patatine fritte', 'Pizza Patatine', 5.50);
 

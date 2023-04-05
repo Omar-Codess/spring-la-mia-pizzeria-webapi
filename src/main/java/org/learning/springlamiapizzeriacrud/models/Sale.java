6d0ab12a9e2a6871302bef0b92217a2935316524
Omar-Codess <omartaki8@gmail.com>
2023-04-05 15:35:26 +0200

    creata classe Sale

---
+++ src/main/java/org/learning/springlamiapizzeriacrud/models/Sale.java
@@ -0,0 +1,63 @@
+package org.learning.springlamiapizzeriacrud.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LocalDate startDate;
+    private LocalDate expireDate;
+    private String title;
+
+    @ManyToOne
+    @NotNull
+    Pizza pizza;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(LocalDate expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+}
